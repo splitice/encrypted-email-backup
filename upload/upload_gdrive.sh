f6e9a76758ac6c94ede5b9230d6d075cfe40e627
splitice <mat999@gmail.com>
2016-02-04 00:50:48 +1100

    bug fix: dont create a new directory each time

--- upload/upload_gdrive.sh
+++ upload/upload_gdrive.sh
@@ -2,7 +2,10 @@
 GDRIVE_FOLDER=${GDRIVE_FOLDER-backups}
 
 function upload_gdrive {
-	FOLDER_ID=$(drive folder --title "$GDRIVE_FOLDER" | grep "Id:" | awk '{print $2}')
+	FOLDER_ID=$(drive list -q "TITLE='backups'" | tail -n +2 | head -n 1 | awk '{print $1}')
+	if [[ "z$FOLDER_ID" == "z" ]]; then
+		FOLDER_ID=$(drive folder --title "$GDRIVE_FOLDER" | grep "Id:" | awk '{print $2}')
+	fi
 	ID=$(drive upload --file "$1" --parent "$FOLDER_ID" | grep "Id:" | awk '{print $2}')
 	drive url --id "$ID"
 }
