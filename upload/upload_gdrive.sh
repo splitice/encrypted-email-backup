c879e99e5c5e7650009749a8cad9d988fa4c4925
splitice <mat999@gmail.com>
2016-02-04 00:26:26 +1100

    fix silly mistake

--- upload/upload_gdrive.sh
+++ upload/upload_gdrive.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 function upload_gdrive {
-	ID=$(drive upload --file "$0" | grep "Id:" | awk '{print $2}')
+	ID=$(drive upload --file "$1" | grep "Id:" | awk '{print $2}')
 	drive url --id "$ID"
 }
